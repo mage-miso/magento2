25081d7047c836721b2b39fe82adccebecad047e
박정훈 <pjh1213@gmail.com>
2017-11-02 16:26:05 +0900

    edit registration.php

--- Mobile/registration.php
+++ Mobile/registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Samsung_Mobile2',
+    'Samsung_Mobile',
     __DIR__
 );
